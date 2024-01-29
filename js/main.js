1a405434680916d12a8e550d874dec3d6ba12efc
LoraStratos <LoraStratosSi@mail.ru>
2024-01-29 18:59:14 +0700

    практическая 7 вычисляемые свойства

--- js/main.js
+++ js/main.js
@@ -5,35 +5,49 @@ let app = new Vue({
         product: "Socks",
         description: "A pair of warm, fuzzy socks",
         selectedVariant: 0,
-        image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: false,
+        onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
-        cart: 0,        
+        cart: 0,
+        
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-        this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
         },
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale(){
+            if(this.onSale === true) {
+                return this.brand + ' ' + this.product + ' on sale';
+            }
         }
+         
     }
 }) 
