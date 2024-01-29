2da1f7e2069cab238de8d04db0a91778f849b35c
LoraStratos <LoraStratosSi@mail.ru>
2024-01-29 15:07:55 +0700

    практическая 6 привязка классов и стилей

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let app = new Vue({
         description: "A pair of warm, fuzzy socks",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: true,
+        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
@@ -27,9 +27,6 @@ let app = new Vue({
         },
         updateProduct(variantImage) {
         this.image = variantImage
-        },
-        remoteToCart() {
-            this.cart -= 1
         },
     }
     
